385eb31f3397c72d9aa6d5cd9591515dd97a35cc
tasfia236 <tasfiatabassum300@gmail.com>
2025-06-24 17:57:09 +0600

    modify pdf

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -18,7 +18,7 @@ const HeroSection = () => {
       <div className={cn('top-4 left-4 z-50 absolute')}>
         <button
           onClick={toggleMenu}
-          className='fixed p-4 rounded-full text-white focus:outline-none'
+          className='fixed p-4 rounded-full focus:outline-none text-white'
         >
           {isMenuOpen ? (
             <span className='font-semibold text-2xl'>✕</span>
@@ -35,7 +35,7 @@ const HeroSection = () => {
         transition={{ duration: 0.5 }}
         className={`fixed top-0 left-0 h-full w-64 bg-slate-800 bg-opacity-60 items-center text-white shadow-lg flex flex-col  justify-center p-6 z-40`}
       >
-        <ul className='space-y-6 font-medium text-center text-lg'>
+        <ul className='space-y-6 font-medium text-lg text-center'>
           <li>
             <a href='/'>HOME</a>
           </li>
@@ -58,16 +58,16 @@ const HeroSection = () => {
       </motion.div>
 
       {/* Main Content */}
-      <section className='absolute flex flex-row justify-between items-center ml-0 md:ml-12 px-8 sm:px-12 lg:px-32 md:xp-20 min-h-screen'>
+      <section className='absolute flex flex-row justify-between items-center ml-0 md:ml-12 px-8 sm:px-12 lg:px-32 min-h-screen md:xp-20'>
         {/* Right Section: Hero Text */}
-        <div className='flex-1 mt-8 ml-0 md:ml-12 text-center md:text-left'>
+        <div className='flex-1 mt-8 ml-0 md:ml-12 md:text-left text-center'>
           <motion.div
             initial={{ opacity: 0, scale: 0.8 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{ duration: 0.8 }}
-            className='opacity-0 transform scale-90'
+            className='opacity-0 scale-90 transform'
           >
-            <h1 className='font-bold text-4xl text-white md:text-5xl'>
+            <h1 className='font-bold text-white text-4xl md:text-5xl'>
               Hi, I'm <span className='text-teal-600'>Tasfia Tabassum</span>
             </h1>
           </motion.div>
@@ -75,12 +75,12 @@ const HeroSection = () => {
             initial={{ y: 50, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
             transition={{ delay: 0.5, duration: 1 }}
-            className='font-bold text-4xl text-black md:text-5xl'
+            className='font-bold text-black text-4xl md:text-5xl'
           >
             <span className='text-teal-500'>Web Developer</span>
           </motion.h1>
 
-          <div className='relative z-50 flex justify-center md:justify-start space-x-4 mt-8'>
+          <div className='z-50 relative flex justify-center md:justify-start space-x-4 mt-8'>
             <motion.a
               initial={{ opacity: 0, scale: 0.8 }}
               animate={{ opacity: 1, scale: 1 }}
@@ -94,9 +94,9 @@ const HeroSection = () => {
               initial={{ opacity: 0, scale: 0.8 }}
               animate={{ opacity: 1, scale: 1 }}
               transition={{ delay: 1.2, duration: 0.5 }}
-              className='flex justify-center items-center gap-2 border-white hover:bg-white px-6 py-2 border rounded font-medium text-white hover:text-black transition'
-              href='/pdf/TASFIA_Resume(FrontEnd).pdf'
-              download='Tasfia Tabassum Resume(Jr. FrontEnd).pdf'
+              className='flex justify-center items-center gap-2 hover:bg-white px-6 py-2 border border-white rounded font-medium text-white hover:text-black transition'
+              href='/pdf/Frontend Resume(Tasfia).pdf'
+              download='Frontend Resume(Tasfia).pdf'
             >
               <FaDownload />
               Download CV
